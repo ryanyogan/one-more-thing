dcf7a86ec04e31842d427292661f3adce12b465c
Ryan Yogan <ryanyogan@gmail.com>
2023-07-08 16:21:39 -0500

    ui ready for auth login

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <div>
-        <h1>home</h1>
-      </div>
-    </main>
-  );
-}
---
+++ src/components/inputs/input.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import clsx from "clsx";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  label: string;
+  id: string;
+  type?: string;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  disabled?: boolean;
+}
+
+export default function Input({
+  label,
+  id,
+  type,
+  register,
+  required,
+  errors,
+  disabled,
+}: InputProps) {
+  return (
+    <div className="">
+      <label
+        className="block text-sm font-medium leading-6 text-gray-900"
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          type={type}
+          disabled={disabled}
+          id={id}
+          autoComplete={id}
+          {...register(id, { required })}
+          className={clsx(
+            "form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-400 sm:text-sm sm:leading-6",
+            errors[id] && "focus:ring-rose-500",
+            disabled && "opacity-50 cursor-default"
+          )}
+        />
+      </div>
+    </div>
+  );
+}
