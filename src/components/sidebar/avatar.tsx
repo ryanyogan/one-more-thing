8085fa08ea3cb80847fd755d7aea8693b3c1ef31
Ryan Yogan <ryanyogan@gmail.com>
2023-07-08 23:52:37 -0500

    show user status

--- src/components/sidebar/avatar.tsx
+++ src/components/sidebar/avatar.tsx
@@ -9,7 +9,7 @@ interface AvatarProps {
 
 export default function Avatar({ user }: AvatarProps) {
   return (
-    <div className="relativd">
+    <div className="relative">
       <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
         <Image
           fill
@@ -17,6 +17,7 @@ export default function Avatar({ user }: AvatarProps) {
           alt="avatar"
         />
       </div>
+      <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />
     </div>
   );
 }
