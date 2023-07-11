e2585388728c56a592874de8cde83fce10bf8a83
Ryan Yogan <ryanyogan@gmail.com>
2023-07-11 18:52:44 -0500

    show them images

--- src/components/messages/message-box.tsx
+++ src/components/messages/message-box.tsx
@@ -5,7 +5,9 @@ import clsx from "clsx";
 import { format } from "date-fns";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useState } from "react";
 import Avatar from "../sidebar/avatar";
+import ImageModal from "./image-modal";
 
 interface MessageBoxProps {
   isLast?: boolean;
@@ -14,6 +16,8 @@ interface MessageBoxProps {
 
 export default function MessageBox({ isLast, data }: MessageBoxProps) {
   const session = useSession();
+
+  const [imageModalOpen, setImageModalOpen] = useState(false);
 
   const isOwn = session?.data?.user?.email === data?.sender?.email;
   const seenList = (data.seen || [])
@@ -46,8 +50,15 @@ export default function MessageBox({ isLast, data }: MessageBoxProps) {
           </div>
         </div>
         <div className={message}>
+          <ImageModal
+            src={data.image}
+            isOpen={imageModalOpen}
+            onClose={() => setImageModalOpen(false)}
+          />
+
           {data.image ? (
             <Image
+              onClick={() => setImageModalOpen(true)}
               alt="iamge"
               height={288}
               width={288}
