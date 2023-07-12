2b5944e5c06e4dd074c4f385e1f48575c04a7c38
Ryan Yogan <ryanyogan@gmail.com>
2023-07-11 23:17:47 -0500

    auth

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,13 +1,11 @@
 import AuthForm from "@/components/forms/auth-form";
-import { BsMeta } from "react-icons/bs";
 export default function Home() {
   return (
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <BsMeta className="mx-auto w-auto text-gray-900" size={48} />
-        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-          Sign in to your account
-        </h2>
+        <h1 className="text-center text-4xl font-bold tracking-tighter text-gray-900/80">
+          Zoo
+        </h1>
       </div>
 
       <AuthForm />
