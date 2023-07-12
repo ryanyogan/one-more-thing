6799c919a897bffab64980151effcc89970a680c
Ryan Yogan <ryanyogan@gmail.com>
2023-07-11 23:47:38 -0500

    auth

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
   return (
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h1 className="text-center text-4xl font-bold tracking-tighter text-gray-900/80">
-          Zoo
+        <h1 className="text-center text-2xl font-bold leading-7 text-gray-900">
+          HERE'S ONE THING
         </h1>
       </div>
 
